5d325d3f298898fd25bc2decfa0158789185b160
pete <50613786+petenull@users.noreply.github.com>
2019-06-22 12:45:34 +1000

    Create security.h

---
+++ security.h
@@ -0,0 +1,20 @@
+
+bool isprocess(const char * const processName)
+{
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	PROCESSENTRY32 pe;
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	Process32First(hSnapshot, &pe);
+
+	while (1) {
+		if (strcmp(pe.szExeFile, processName) == 0) return true;
+		if (!Process32Next(hSnapshot, &pe)) return false;
+	}
+}
+
+if (isprocess("program.exe"))
+	{
+		//MessageBox(0, "body", "title", MB_OK);
+		exit(1);
+	}
